44c2c024f9762d853ccddc5396f1e0c4d1222135
GerardGurgui <105665705+GerardGurgui@users.noreply.github.com>
2022-06-20 13:54:27 +0200

    Update App.java

--- src/n1Ex1/App.java
+++ src/n1Ex1/App.java
@@ -5,7 +5,7 @@ public class App {
      public static void main(String[] args) {
         
         
-        Persona persona = new Persona("Persona3(2)", "Persona2(1)", "Persona1(3)");
+         Persona persona = new Persona("Persona3(2)", "Persona2(1)", "Persona1(3)");
        
          System.out.println("persona 1 = " +persona.getPersona1());
          System.out.println("persona 2 = " +persona.getPersona2());
